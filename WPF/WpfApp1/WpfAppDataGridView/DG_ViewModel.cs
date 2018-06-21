b46e69eb9529a894c9cc166226d287d68264f8f0
YonedaMasashi <pillows916@gmail.com>
2018-06-22 02:00:43 +0900

    WPFサンプル集追加

---
+++ WPF/WpfApp1/WpfAppDataGridView/DG_ViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDataGridView {
+    public class ValueList {
+        public List<DG_ViewModel> DG_ViewModels { get; set; }
+        public ValueList() {
+            DG_ViewModels = new List<DG_ViewModel>() {
+                new DG_ViewModel { Slot = 1, Data1=1, Data2=1, Execute=false},
+                new DG_ViewModel { Slot = 2, Data1=1, Data2=0, Execute=false},
+                new DG_ViewModel { Slot = 3, Data1=1, Data2=2, Execute=false},
+            };
+        }
+    }
+
+    public class DG_ViewModel : ViewModelBase {
+
+        private long _Slot = 0;
+        public long Slot {
+            get {
+                return _Slot;
+            }
+            set {
+                _Slot = value;
+                OnPropertyChanged("Slot");
+            }
+        }
+
+        private bool _Execute = false;
+        public bool Execute {
+            get {
+                return _Execute;
+            }
+            set {
+                _Execute = value;
+                OnPropertyChanged("Execute");
+            }
+        }
+
+        private long _Data1 = 0;
+        public long Data1 {
+            get {
+                return _Data1;
+            }
+            set {
+                _Data1 = value;
+                UpdateIsEnableExecute();
+                OnPropertyChanged("Data1");
+            }
+        }
+
+        private long _Data2 = 0;
+        public long Data2 {
+            get {
+                return _Data2;
+            }
+            set {
+                _Data2 = value;
+                UpdateIsEnableExecute();
+                OnPropertyChanged("Data2");
+            }
+        }
+
+        private bool _IsEnableExecute = false;
+        public bool IsEnableExecute {
+            get {
+                return _IsEnableExecute;
+            }
+            set {
+                _IsEnableExecute = value;
+                OnPropertyChanged("IsEnableExecute");
+            }
+        }
+
+        void UpdateIsEnableExecute() {
+            if (_Data1 != 0 && _Data2 != 0) {
+                IsEnableExecute = true;
+            } else {
+                IsEnableExecute = false;
+            }
+        }
+    }
+}
---
+++ WPF/WpfApp1/WpfAppDataGridView/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDataGridView {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            CellValue.DataContext = new ValueList();
+        }
+
+        private void Hoge_Click(object sender, RoutedEventArgs e) {
+            var values = (ValueList)CellValue.DataContext;
+            foreach (var elem in values.DG_ViewModels) {
+                var data = elem.Execute;
+            }
+        }
+    }
+}
