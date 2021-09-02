74587b6a98e1fabd28892431ff40c46b0a70042b
YonedaMasashi <pillows916@gmail.com>
2021-09-02 23:02:13 +0900

    Utf8JsonSampleを追加

---
+++ CSharp/Utf8JsonSample/Utf8JsonSampleh/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Utf8JsonSampleh")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Utf8JsonSampleh")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9802f94a-48da-4295-bdbb-2891ddb84872")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharp/Utf8JsonSample/Utf8JsonSampleh/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utf8JsonSampleh
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var root = new Dictionary<string, object>();
+
+            var child = new List<object>();
+            foreach (var i in Enumerable.Range(1, 10))
+            {
+                child.Add(i);
+            }
+
+            root.Add("Root", child);
+
+            var hoge = Utf8Json.JsonSerializer.ToJsonString(root);
+            
+
+            Assert.AreEqual("", hoge);
+        }
+    }
+}
